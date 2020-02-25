ffb4950fa76fe22250f9b1cafd80473d3840077e
skilletComatose <romariomarimon21@gmail.com>
2020-02-25 16:05:39 -0500

    avance

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
 FROM python:3.6-buster
 RUN pip3 install Flask
+RUN mkdir templates
+RUN apt update
+RUN apt install sqlite3 -y
+RUN mkdir basedatos/
+
+COPY basedatos/tasks.db basedatos/
 COPY hello.py /
-COPY Templates/* /    
+
+COPY templates/* /templates
+RUN pip3 install Flask-SQLAlchemy 
 
 ENV FLASK_APP=hello.py
 ENV FLASK_ENV=development
--- hello.py
+++ hello.py
@@ -1,108 +1,33 @@
-from flask import Flask,render_template ,url_for,request, jsonify
+from flask import Flask,render_template,redirect,url_for,request
+from flask_sqlalchemy import SQLAlchemy
 
-import sqlite3 as sql
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///basedatos/tasks.db'
+db = SQLAlchemy(app)
 
-#hashes = []
+class Datos(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    hash=db.Column(db.String(100))
+    
 
-
-def CreateTable(): 
-    conn = sql.connect('database.db')
-    conn.execute('CREATE TABLE IF NOT EXISTS hashes (HASH text )')
-    print("table created")
-    conn.close()
-
-CreateTable()
 
 @app.route("/")
-def main():
+def home():
     return render_template('index.html')
 
-
-@app.route('/chain',  methods=['GET','POST'] )
-def InsertHash():
-    if request.method == 'POST':
-        new_hash = request.form['h']
+@app.route('/chain', methods=['POST','GET'])
+def guardar():
+    if(request.method=='POST'):
+        has=Datos(hash=request.form['contenedor'])
+        db.session.add(has)
+        db.session.commit()
+        return redirect(url_for('home'))
     
-        try:                                                             
-                                    
-            with sql.connect("database.db") as con:
-
-                cur = con.cursor()                                      
-                                                                        
-                cur.execute('INSERT INTO hashes (HASH) VALUES (?)',(new_hash))
-                
-                con.commit()
-
-                mensaje = "    *************    Successful ************* "
-
-        except:
-            con.rollback()
-            mensaje = "        *************      Error    ************** "
-
-        finally:
-            con.close()
-           
-            return render_template("resultado.html",msg = mensaje)
-
-    
-        def Show():
-             con = sql.connect('database.db')
-             con.row_factory = sql.Row
-             cur = con.cursor()
-             cur.execute('select HASH from hashes')
-             registers_saved = cur.fetchall()
-             return render_template('index.html',rows =  registers_saved)     
-    
-    return render_template('insert.html')
-    
-
-'''
-@app.route("/chain, methods=['GET','POST']")
-
-
-def InsertHash():
-    if request.method == 'POST':
+    if(request.method=='GET'):
+        datos= Datos.query.all()
+        return render_template('index.html',datos=datos)
         
-        try:                                                            #detecta un error 
-                                      #obtiene los datos del form
-            nam = request.form['NAME']
-            Vhash = request.form['HASH']
-
-            with sql.connect("database.db") as con:
-
-                cur = con.cursor()                                      #prepara in cursor
-                                                                        #ejecuta insercion
-                cur.execute('INSERT INTO hashes (ID,NAME,HASH) VALUES (?,?,?)',(nam,Vhash))
-                
-                con.commit()
-
-                mensaje = "mensaje exitoso "
-
-        except:
-                                                                        #hace un rollback debido al error
-            con.rollback()
-            mensaje = "error en la insercion"
-
-        finally:
-
-            con.close()
-            #renderiza a template de resultados 
-            return render_template("resultado.html",msg = mensaje)
-
     
-          
 
-
-@app.route('/chain', methods=['GET', 'POST']) #decorador, metodos permitidos para esa ruta  
-def chain():
-    if request.method == 'POST':                # validar metodo 
-        new_hash = request.args.get('hash')
-        if new_hash is None:                   #verificar que haya un hash
-            return ""
-        #hashes.append(new_hash)                #agrega nuevo hash
-    else:
-       # return jsonify(hashes)
-'''
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ =='__main__':
+    app.run(debug=True)
